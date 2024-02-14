51d3f43220516f355adcade406cd3cf9a2a6f0e6
Manuel Pietschmann <mpi@dresden-elektronik.de>
2024-02-14 15:46:46 +0100

    Add u_memory module
    Common memory functions to abstract usage of libc.

---
+++ deconz/u_memory.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024 dresden elektronik ingenieurtechnik gmbh.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ *
+ */
+
+#ifndef U_MEMORY_H
+#define U_MEMORY_H
+
+#ifndef U_LIBAPI
+#ifdef _WIN32
+#ifdef USE_ULIB_SHARED
+    #define U_LIBAPI  __declspec(dllimport)
+  #endif
+  #ifdef BUILD_ULIB_SHARED
+    #define U_LIBAPI  __declspec(dllexport)
+  #endif
+#endif
+#endif /* ! defined(U_LIBAPI) */
+
+#ifndef U_LIBAPI
+#define U_LIBAPI
+#endif
+
+
+#define U_KILO_BYTES(n) ((n) * 1000)
+#define U_MEGA_BYTES(n) ((n) * 1000 * 1000)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+U_LIBAPI void *U_memalign(void *p, unsigned align);
+U_LIBAPI void *U_memset(void *p, int c, unsigned long n);
+U_LIBAPI int U_memcmp(const void *a, const void *b, unsigned long n);
+U_LIBAPI void *U_memcpy(void *dst, const void *src, unsigned long n);
+
+U_LIBAPI void *U_Alloc(unsigned long size);
+U_LIBAPI int U_Free(void *p);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* U_MEMORY_H */
---
+++ u_memory.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 dresden elektronik ingenieurtechnik gmbh.
+ * All rights reserved.
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ *
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "u_assert.h"
+#include "u_memory.h"
+
+void *U_memalign(void *p, unsigned align)
+{
+    uintptr_t num;
+    uintptr_t algn;
+    void *p1;
+
+    U_ASSERT(align == 1 || align == 8 || align == 16 || align == 32 || align == 64);
+
+    num = (uintptr_t)p;
+    algn = align;
+    p1 = (void*)((num + (algn - 1)) & ~(algn - 1));
+
+    U_ASSERT(p <= p1);
+    return p1;
+}
+
+void *U_memset(void *p, int c, unsigned long n)
+{
+    return memset(p, c, n);
+}
+
+int U_memcmp(const void *a, const void *b, unsigned long n)
+{
+    return memcmp(a, b, n);
+}
+
+void *U_memcpy(void *dst, const void *src, unsigned long n)
+{
+    return memcpy(dst, src, n);
+}
+
+void *U_Alloc(unsigned long size)
+{
+    void *p;
+
+    p = malloc(size);
+    if (p)
+    {
+        U_memset(p, 0, size);
+    }
+
+    return p;
+}
+
+int U_Free(void *p)
+{
+    U_ASSERT(p != 0);
+
+    if (p)
+    {
+        free(p);
+        return 1;
+    }
+    return 0;
+}
