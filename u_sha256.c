aedf77f0018b2542405b5c59f2b22281f36a3828
Manuel Pietschmann <mpi@dresden-elektronik.de>
2024-05-30 12:46:28 +0200

    Use lonesha256 as fallback if OpenSSL can't be resolved
    Sometimes there is trouble getting OpenSSL to load. This fallback to single header library version lets SHA-256 hash work anyway.

--- u_sha256.c
+++ u_sha256.c
@@ -1,10 +1,17 @@
+#include <stddef.h>
 #include "deconz/u_sha256.h"
 #include "deconz/u_library_ex.h"
 
-#ifdef HAS_OPENSSL
-#include <openssl/sha.h>
+#define LONESHA256_STATIC
+#include "lonesha256.h"
 
 static unsigned char (*libSHA256)(const unsigned char *d, size_t n, unsigned char *md);
+
+unsigned char libSha256LoneHash(const unsigned char *d, size_t n, unsigned char *hash)
+{
+    lonesha256(hash, d, n);
+    return 1;
+}
 
 static int init_sha256_lib(void)
 {
@@ -12,9 +19,14 @@ static int init_sha256_lib(void)
 
     if (!libSHA256)
     {
+#ifdef HAS_OPENSSL
         libcrypto = U_library_open_ex("libcrypto");
         if (libcrypto)
             libSHA256 = (unsigned char (*)(const unsigned char*, size_t, unsigned char*))U_library_symbol(libcrypto, "SHA256");
+#endif
+
+        if (!libSHA256)
+            libSHA256 = libSha256LoneHash;
     }
 
     return libSHA256 ? 1 : 0;
@@ -31,15 +43,3 @@ int U_Sha256(const void *data, unsigned size, unsigned char *hash)
     libSHA256(data, size, hash);
     return 1;
 }
-
-#else
-
-int U_Sha256(const void *data, unsigned size, unsigned char *hash)
-{
-    (void)data;
-    (void)size;
-    (void)hash;
-    return 0;
-}
-
-#endif
