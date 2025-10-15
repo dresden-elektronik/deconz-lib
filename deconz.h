999b73602a164f8735c16bfe87171f79870436b1
Manuel Pietschmann <mpi@dresden-elektronik.de>
2025-10-15 14:54:01 +0200

    Bump lib version to 0x011204

--- deconz.h
+++ deconz.h
@@ -2,7 +2,7 @@
 #define DECONZ_H
 
 /*
- * Copyright (c) 2012-2023 dresden elektronik ingenieurtechnik gmbh.
+ * Copyright (c) 2012-2025 dresden elektronik ingenieurtechnik gmbh.
  * All rights reserved.
  *
  * The software in this package is published under the terms of the BSD
@@ -14,7 +14,7 @@
 /*! deCONZ library version as numeric value of the form:
     0xMMNNPP (MM = major, NN = minor, PP = patch).
  */
-#define DECONZ_LIB_VERSION 0x011104
+#define DECONZ_LIB_VERSION 0x011204
 
 #include <deconz/declspec.h>
 #include <deconz/types.h>
